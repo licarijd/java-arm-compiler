6d3f45d99db6024130341b7b838b204127b79f38
Justin Licari <Justin@Justins-MacBook-Air.local>
2018-02-19 14:27:16 -0500

    Added Java grammar for simple Java programs consisting of only a main method and print statement which prints a String in rdparser.py

--- rdparser.py
+++ rdparser.py
@@ -1,1 +1,8 @@
 
+#Java grammar for simple Java programs consisting of only a main method and print statement which prints a String
+
+#PROG --> (IMPORTS identifier)* CLASS_DECLARATION
+#CLASS_DECLARATION --> 'public class' identifier '{' MAIN_METHOD_DECLARATION '}'
+#MAIN_METHOD_DECLARATION --> 'public static void main (String[] args) {' METHOD_BODY '}'
+#METHOD_BODY --> PRINT_STATEMENT '(' STRING ')'
+#STRING --> \" . \"
