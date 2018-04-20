6508d76eaeb1a786143eaffa34f0bf1c81a2067c
Justin Licari <Justin@Justins-MacBook-Air.local>
2018-04-19 20:02:39 -0400

    add symbols to text file for parser input

--- scanner.py
+++ scanner.py
@@ -1,9 +1,15 @@
 import sys
+import fileinput
 
 currentPosition = 0;
 currentBlockPosition = 0;
 
 def main():
+
+	#Clear the output file which the scanner symbols will be written to
+	f = open('symbols.txt','w')
+	f.write('')
+	f.close()
 
 	commentBlock = False;
 
@@ -34,7 +40,11 @@ def main():
 	try:
 		getSym(words)
 	except:
-		print("Error")
+		with open('symbols.txt', 'r') as fin:
+    			data = fin.read().splitlines(True)
+		with open('symbols.txt', 'w') as fout:
+    			fout.writelines(data[1:])
+		print("Scanning Complete")
 	
 	
 
@@ -54,6 +64,12 @@ def getSym(words):
 
 	#Once all symbols have been checked, start parsing the scanned symbols.
 	else :
+
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'EofSym')
+		f.close()
+
 		sys.stdout.write("EofSym\n");
 		sys.exit()
 
@@ -69,106 +85,218 @@ def checkForReservedWords(sym, words):
 	sym = words[currentPosition]
 
 	if sym == "not":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'NotSym')
+		f.close()
+
 		sys.stdout.write("NotSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "or":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'OrSym')
+		f.close()
+		
 		sys.stdout.write("OrSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "and":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'AndSym')
+		f.close()
+
 		sys.stdout.write("AndSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "of":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'OrSym')
+		f.close()
+
 		sys.stdout.write("OfSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "then":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'ThenSym')
+		f.close()
+
 		sys.stdout.write("ThenSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "do":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'DoSym')
+		f.close()
+
 		sys.stdout.write("DoSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "not":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'NotSym')
+		f.close()
+
 		sys.stdout.write("NotSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "if":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'IfSym')
+		f.close()
+
 		sys.stdout.write("IfSym\n");
 		currentPosition+=1;
 		getSym(words)
-	elif sym == "procedure":
-		sys.stdout.write("ProcedureSym\n");
-		currentPosition+=1;
-		getSym(words)
 	elif sym == "exit":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'ExitSym')
+		f.close()
+
 		sys.stdout.write("ExitSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "else":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'ElseSym')
+		f.close()
+
 		sys.stdout.write("ElseSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "while":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'WhileSym')
+		f.close()
+
 		sys.stdout.write("WhileSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "{":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'LCurlSym')
+		f.close()
+
 		sys.stdout.write("LCurlSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "}":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'RCurlSym')
+		f.close()
+
 		sys.stdout.write("RCurlSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "public":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'PublicSym')
+		f.close()
+
 		sys.stdout.write("PublicSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "static":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'StaticSym')
+		f.close()
+
 		sys.stdout.write("StaticSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "void":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'VoidSym')
+		f.close()
+
 		sys.stdout.write("VoidSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "main":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'MainSym')
+		f.close()
+
 		sys.stdout.write("MainSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "[]":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'ArraySym')
+		f.close()
+
 		sys.stdout.write("ArraySym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "String":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'StringSym')
+		f.close()
+
 		sys.stdout.write("StringSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "args":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'ArgsSym')
+		f.close()
+
 		sys.stdout.write("ArgsSym\n");
 		currentPosition+=1;
 		getSym(words)
-	elif sym == "becomes":
-		sys.stdout.write("BecomesSym\n");
-		currentPosition+=1;
-		getSym(words)
 	elif sym == "System":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'SystemSym')
+		f.close()
+
 		sys.stdout.write("SystemSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "out":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'OutSym')
+		f.close()
+
 		sys.stdout.write("OutSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "println":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'PrintSym')
+		f.close()
+
 		sys.stdout.write("PrintSym\n");
 		currentPosition+=1;
 		getSym(words)
 	elif sym == "class":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'ClassSym')
+		f.close()
+
 		sys.stdout.write("ClassSym\n");
 		currentPosition+=1;
 		getSym(words)
@@ -176,6 +304,11 @@ def checkForReservedWords(sym, words):
 		symList = list(words[currentPosition])
 		checkForReservedSymbols(symList,words)
 	else:
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'IdentSym')
+		f.close()
+
 		sys.stdout.write("IdentSym\n");
 		currentPosition+=1;
 		getSym(words)
@@ -196,66 +329,137 @@ def checkForReservedSymbols(symList,words):
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == "*":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'TimesSym')
+			f.close()
+
 			sys.stdout.write("TimesSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == "/":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'DivSym')
+			f.close()
+
 			sys.stdout.write("DivSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == "%":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'ModSym')
+			f.close()
+
 			sys.stdout.write("ModSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == "&":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'AndSym')
+			f.close()
+
 			sys.stdout.write("AndSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == "+":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'PlusSym')
+			f.close()
+
 			sys.stdout.write("PlusSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == "-":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'MinusSym')
+			f.close()
+
 			sys.stdout.write("MinusSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
-		elif sym == "&":
-			sys.stdout.write("AndSym\n");
-			currentBlockPosition+=1;
-			checkForReservedSymbols(symList,words)
 		elif sym == "|":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'OrSym')
+			f.close()
+
 			sys.stdout.write("OrSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == ".":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'PeriodSym')
+			f.close()
+
 			sys.stdout.write("PeriodSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == ",":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'CommaSym')
+			f.close()
+
 			sys.stdout.write("CommaSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == ":":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'ColonSym')
+			f.close()
+
 			sys.stdout.write("ColonSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == "(":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'RparenSym')
+			f.close()
+
 			sys.stdout.write("RparenSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == ")":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'LparenSym')
+			f.close()
+
 			sys.stdout.write("LparenSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == "[":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'LbrakSym')
+			f.close()
+
 			sys.stdout.write("LbrakSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == "]":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'RbrakSym')
+			f.close()
+
 			sys.stdout.write("RbrakSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
 		elif sym == ";":
+			#Append the symbol to the output file
+			f = open('symbols.txt','a')
+			f.write('\n' + 'SemicolonSym')
+			f.close()
+
 			sys.stdout.write("SemicolonSym\n");
 			currentBlockPosition+=1;
 			checkForReservedSymbols(symList,words)
@@ -279,26 +483,56 @@ def checkRelationalOp(words):
 	next = list(words[currentPosition])
 
 	if sym == "!" and next == "=":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'NeqSym')
+		f.close()
+
 		sys.stdout.write("NeqSym\n");
 		currentBlockPosition+=2;
 		checkForReservedSymbols(symList,words)
 	elif sym == "<" and next == "=":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'LeqSym')
+		f.close()
+
 		sys.stdout.write("LeqSym\n");
 		currentBlockPosition+=2;
 		checkForReservedSymbols(symList,words)
 	elif sym == ">" and next == "=":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'GeqSym')
+		f.close()
+
 		sys.stdout.write("GeqSym\n");
 		currentBlockPosition+=2;
 		checkForReservedSymbols(symList,words)
 	elif sym == ">":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'GtrSym')
+		f.close()
+
 		sys.stdout.write("GtrSym\n");
 		currentBlockPosition+=2;
 		checkForReservedSymbols(symList,words)
 	elif sym == "<" and next == "=":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'LssSym')
+		f.close()
+
 		sys.stdout.write("LssSym\n");
 		currentBlockPosition+=2;
 		checkForReservedSymbols(symList,words)
 	elif sym == "=":
+		#Append the symbol to the output file
+		f = open('symbols.txt','a')
+		f.write('\n' + 'EqlSym')
+		f.close()
+
 		sys.stdout.write("EqlSym\n");
 		currentBlockPosition+=2;
 		checkForReservedSymbols(symList,words)
